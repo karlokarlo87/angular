f1a9ebdaa2a4beb33d82ff288cdc04665a99fb41
karlokarlo87 <k.firanishvili@yahoo.com>
2022-02-07 23:33:41 +0400

    no message

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ListComponent } from './list/list.component';
+const routes: Routes = [
+  {path:'admin',children:[
+    {path:'list',component:ListComponent},
+    {path:'login',component:LoginComponent}
+  ]}
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
